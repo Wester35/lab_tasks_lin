73b0b47e62c927475e8a9d3449718a95734242c1
Wester35 <shum0302@gmail.com>
2023-10-12 18:14:27 +0300

    lab 4

---
+++ lab4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    const int row = 4, column = 7;
+    int matrix[row][column], min;
+    bool check;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            matrix[i][j] = rand() % 6 - 1;
+
+    min = matrix[0][0];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            printf("%4d ", matrix[i][j]);
+        printf("\n");
+    }
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            if (min > matrix[i][j])
+                min = matrix[i][j];
+
+    for (int j = 0; j < column; j++)
+    {
+        check = false;
+        for (int i = 0; i < row; i++)
+            if (matrix[i][j] == min)
+                check = true;
+        if (check)
+            printf("Column %d\n", j);
+    }
+
+    return 0;
+}
