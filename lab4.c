b9981518833f3249566cd8a3173de22af83c3576
Wester35 <shum0302@gmail.com>
2023-10-25 14:57:16 +0300

    lab4-8

--- lab4.c
+++ lab4.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
+int main() {
     const int row = 4, column = 7;
     int matrix[row][column], min;
 
@@ -14,8 +14,7 @@ int main(){
 
     min = matrix[0][0];
 
-    for (int i = 0; i < row; i++)
-    {
+    for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++)
             printf("%4d", matrix[i][j]);
         printf("\n");
@@ -28,7 +27,7 @@ int main(){
 
     for (int j = 0; j < column; j++)
         for (int i = 0; i < row; i++)
-            if (matrix[i][j] == min){
+            if (matrix[i][j] == min) {
                 printf("Column %d\n", j);
                 break;
             }
