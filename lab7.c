c06f681d6069486928e62f550afe5dc85231f8e2
Wester35 <shum0302@gmail.com>
2023-11-14 16:52:28 +0300

    lab777

--- lab7.c
+++ lab7.c
@@ -3,14 +3,33 @@
 #include <string.h>
 
 int main() {
-    int asize;
+    int asize = 100, count = 0;
 
-    printf("Enter size: ");
-    scanf("%d", &asize);
+    // printf("Enter size: ");
+    // scanf("%d", &asize);
 
-    char* arr = (char*) malloc(sizeof(int) * asize);
+    char* arr = (char*) malloc(sizeof(char) * asize);
 
     fgets(arr, asize, stdin);
 
-    printf("%s", arr);
+    //arr = (char*)realloc(strlen(arr));
+    // for (int i = 0; i < asize; i++) {
+    //     if (strtok(arr, " ") == 3){
+    //         k++;
+    //     }
+    // }
+    arr = strtok(arr," ,.");
+    while(arr){
+        count++;
+        
+        //printf("size word %i: %i\n", count, strlen(arr));
+        if (strlen(arr) == 3){
+            printf("%s ", arr);
+        }
+        arr = strtok(NULL," ,.");
+
+    }
+    free(arr);
+    
+    return 0;
 }
