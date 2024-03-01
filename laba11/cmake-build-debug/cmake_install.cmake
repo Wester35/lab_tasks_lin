31df1bca0f4a4e56b75ef812adabb7083cb1d766
Wester35 <shum0302@gmail.com>
2024-03-01 17:07:52 +0300

    0103v2

--- laba11/cmake-build-debug/cmake_install.cmake
+++ laba11/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Wstr35/Desktop/lab_tasks_lin/laba11
+# Install script for directory: C:/Users/wstr/Desktop/lab_tasks_lin/laba11
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Wstr35/Desktop/lab_tasks_lin/laba11/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/wstr/Desktop/lab_tasks_lin/laba11/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
